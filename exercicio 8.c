00d16a08ba48b5b841bf65e035c28b5218c47345
sabrina silva <102616948+sabrina702@users.noreply.github.com>
2022-06-22 14:53:40 -0300

    Add files via upload

---
+++ exercicio 8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int alt(int alt){
+	int sexo;
+	float peso;
+	switch(sexo){
+	case 1: 
+		peso = 62.1 * alt - 44.7;
+		break;
+	
+	case 2:
+		peso = 72.7 * alt - 58;
+		break;
+	}
+	return peso;
+}
+ int main(){
+ 	int sexo;
+ 	float altura,ideal;
+ 	printf("qual seu genero 1-para feminino e 2-para masculino: ");
+ 	scanf("%d",&sexo);
+ 	printf("informe sua altura: ");
+ 	scanf("%f",&altura);
+ 	ideal = alt(altura);
+ 	printf("peso ideal: %f",ideal);
+ 	return 0;
+ }
