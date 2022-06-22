688280beb6ba56f56ab0470e4012b6ec538ac062
sabrina silva <102616948+sabrina702@users.noreply.github.com>
2022-06-22 10:26:01 -0300

    Add files via upload

---
+++ busca.c
@@ -0,0 +1,28 @@
+int main(){
+	int i, vet[100], n, busca;
+	float soma=0;
+	printf("Quantidade de valores no vetor:");
+	scanf("%d",&n);	
+	// lendo dados para o vetor
+	for(i=0;i<n;i++){
+	   printf("Vetor[%d]:",i);
+	   scanf("%d",&vet[i]);
+	}   
+	// mostrando os dados do vetor
+	printf("*** Dados do Vetor ***\n");
+	for(i=0;i<n;i++){
+	   printf("[%d] %d |",i, vet[i]);
+	   soma+= vet[i];
+	}  
+	printf("\nsoma do vetor:%.2f\n",soma);
+	printf("media:%.2f\n",soma);
+	// busca
+		printf("valor de busca:");
+	scanf("%d",&busca);
+	for(i=0;i<n;i++){
+		if(busca==vet[i]){
+			printf("valor %d na posicao %d\n",busca,i);
+		}
+	return 0;
+}
+}
